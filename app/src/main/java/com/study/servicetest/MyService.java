f550d06ccdd31c0fa8d1d96c99540442d905fd65
WXB506 <1922686887@qq.com>
2016-03-09 22:43:19 +0800

    initial commit

---
+++ app/src/main/java/com/study/servicetest/MyService.java
@@ -0,0 +1,31 @@
+package com.study.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by WXB506 on 2016/3/9.
+ */
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
