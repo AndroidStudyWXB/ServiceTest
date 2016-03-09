9fd2b2485b9479931d6f7001e786f189d3a2bfb6
WXB506 <1922686887@qq.com>
2016-03-09 22:55:14 +0800

    service start and stop

--- app/src/main/java/com/study/servicetest/MainActivity.java
+++ app/src/main/java/com/study/servicetest/MainActivity.java
@@ -1,13 +1,41 @@
 package com.study.servicetest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private Button startService;
+    private Button stopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startService = (Button) findViewById(R.id.start_service);
+        startService.setOnClickListener(this);
+
+        stopService = (Button) findViewById(R.id.stop_service);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+            default:
+                break;
+        }
     }
 }
